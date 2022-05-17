0a9507f15825fda8e125a997a8f668914d81b554
matthew jio <matthew.p.jio1@gmail.com>
2022-05-17 12:21:24 -0500

    push before lunch

--- main.js
+++ main.js
@@ -21,6 +21,7 @@ function renderCoffees(coffees) {
 function updateCoffees(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
     var selectedRoast = roastSelection.value;
+
     var filteredCoffees = [];
     coffees.forEach(function(coffee) {
         if (coffee.roast === selectedRoast) {
