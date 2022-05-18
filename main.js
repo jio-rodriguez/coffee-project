c694f4fe69cc21a3ddd23d7296ee30fa3834940b
Alex Rodriguez <alexander.joshua.rodriguez@gmail.com>
2022-05-18 13:34:18 -0500

    update

--- main.js
+++ main.js
@@ -42,50 +42,75 @@ function updateCoffeesByName(e) {
     tbody.innerHTML = renderCoffees(filteredCoffees);
 }
 
+function updateCoffeesByRoast(e) {
+    e.preventDefault(); // don't submit the form, we just want to update the data
+    var selectedRoast = roastSelection.value;
+    var filteredCoffees = [];
+    coffees.forEach(function(coffee) {
+        if (coffee.roast === selectedRoast || selectedRoast === 'all') {
+            filteredCoffees.push(coffee);
+        }
+    });
+    tbody.innerHTML = renderCoffees(filteredCoffees);
+}
+
+function addCoffee(e){
+    e.preventDefault(); // don't submit the form, we just want to update the data
+    let addedRoastType = roastTypeInput.value;
+    let addedRoastName = roastInput.value
+    var filteredCoffees = [];
+    let idCount = []
+    coffees.forEach(function (coffee) {
+        idCount.push(coffee.id)
+    })
+    let idMax = Math.max(...idCount)
+    let addedCoffee = {
+        id: idMax + 1,
+        name: addedRoastName,
+        roast: addedRoastType,
+        category: 'all'
+    }
+    coffees.push(addedCoffee);
+    console.log(coffees)
+    coffees.forEach(function (coffee) {
+        filteredCoffees.push(coffee)
+    })
+    tbody.innerHTML = renderCoffees(filteredCoffees);
 
 
-// JavaScript code
-// function search_animal() {
-//     let input = document.getElementById('search').value
-//     input=input.toLowerCase();
-//     let x = document.getElementsByClassName('animals');
-//
-//     for (i = 0; i < x.length; i++) {
-//         if (!x[i].innerHTML.toLowerCase().includes(input)) {
-//             x[i].style.display="none";
-//         }
-//         else {
-//             x[i].style.display="list-item";
-//         }
-//     }
-// }
+}
+
 
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
-    {id: 1, name: 'Light City', roast: 'light'},
-    {id: 2, name: 'Half City', roast: 'light'},
-    {id: 3, name: 'Cinnamon', roast: 'light'},
-    {id: 4, name: 'City', roast: 'medium'},
-    {id: 5, name: 'American', roast: 'medium'},
-    {id: 6, name: 'Breakfast', roast: 'medium'},
-    {id: 7, name: 'High', roast: 'dark'},
-    {id: 8, name: 'Continental', roast: 'dark'},
-    {id: 9, name: 'New Orleans', roast: 'dark'},
-    {id: 10, name: 'European', roast: 'dark'},
-    {id: 11, name: 'Espresso', roast: 'dark'},
-    {id: 12, name: 'Viennese', roast: 'dark'},
-    {id: 13, name: 'Italian', roast: 'dark'},
-    {id: 14, name: 'French', roast: 'dark'},
+    {id: 1, name: 'Light City', roast: 'light', category: 'all'},
+    {id: 2, name: 'Half City', roast: 'light', category: 'all'},
+    {id: 3, name: 'Cinnamon', roast: 'light', category: 'all'},
+    {id: 4, name: 'City', roast: 'medium', category: 'all'},
+    {id: 5, name: 'American', roast: 'medium', category: 'all'},
+    {id: 6, name: 'Breakfast', roast: 'medium', category: 'all'},
+    {id: 7, name: 'High', roast: 'dark', category: 'all'},
+    {id: 8, name: 'Continental', roast: 'dark', category: 'all'},
+    {id: 9, name: 'New Orleans', roast: 'dark', category: 'all'},
+    {id: 10, name: 'European', roast: 'dark', category: 'all'},
+    {id: 11, name: 'Espresso', roast: 'dark', category: 'all'},
+    {id: 12, name: 'Viennese', roast: 'dark', category: 'all'},
+    {id: 13, name: 'Italian', roast: 'dark', category: 'all'},
+    {id: 14, name: 'French', roast: 'dark', category: 'all'},
 ];
 
 var tbody = document.querySelector('#coffees');
 var submitButton = document.querySelector('#submit');
 var roastSelection = document.querySelector('#roast-selection');
 let searchBar = document.querySelector('#search')
-
+let roastTypeInput = document.querySelector("#roast-selection-add")
+let roastInput = document.querySelector('#addedCoffee')
+let submitCoffee = document.querySelector("#submitForAdd")
 tbody.innerHTML = renderCoffees(coffees);
 
 submitButton.addEventListener('click', updateCoffees);
+roastSelection.addEventListener("change",updateCoffeesByRoast )
 searchBar.addEventListener("input", updateCoffeesByName)
+submitCoffee.addEventListener("click", addCoffee)
 
